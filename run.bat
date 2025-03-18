57d9e81f17f48fd2ffc44d49efb48b889416908f
isaccbouwsacc <bcmain.main@gmail.com>
2025-03-18 10:58:45 +0100

    Update run.bat
    Transferred logic to .py wrapper

--- run.bat
+++ run.bat
@@ -1,26 +1,3 @@
 @echo off
-
-:: Activate virtual environment
 call .venv\Scripts\activate.bat
-
-set LAUNCH_ARGS=--api
-
-:: Start proxy first
-start /b python ".\modules\secure_proxy.py"
-timeout /t 2 > nul
-
-:: Run the main application
-python ".\modules\webui.py" %LAUNCH_ARGS%
-
-:: When main application exits, kill the proxy server using its PID
-if exist proxy_pid.txt (
-    set /p PROXY_PID=<proxy_pid.txt
-    taskkill /f /pid %PROXY_PID% > nul 2>&1
-    del proxy_pid.txt
-    echo Proxy server shut down.
-) else (
-    echo Proxy PID file not found.
-)
-
-:: Deactivate virtual environment
-call deactivate
+python run_app.py
