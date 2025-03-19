a410818cba697befef9afd4d8e230b49484c3dac
isaccbouwsacc <bcmain.main@gmail.com>
2025-03-19 08:46:13 +0100

    installer.bat aktualisieren

--- installer.bat
+++ installer.bat
@@ -1,6 +1,7 @@
 @echo off
+::venv aktivieren
 call .venv\Scripts\activate
-
+::installation von inhalten von requirements.txt
 pip install -r requirements.txt
-
+::venv deaktivieren
 call deactivate
